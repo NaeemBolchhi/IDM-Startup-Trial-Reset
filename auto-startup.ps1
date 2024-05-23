f17b6de26794b0e4b178a312c6a2c5c9ec4256d9
NaeemBolchhi <naeembolchhi.dev@gmail.com>
2024-05-24 02:45:26 +0600

    attempt fix 2

--- auto-startup.ps1
+++ auto-startup.ps1
@@ -1,12 +1,8 @@
-# Enable TLSv1.2 for compatibility with older clients
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
-
 $DownloadURL = 'https://raw.githubusercontent.com/NaeemBolchhi/IDM-Startup-Trial-Reset/main/IAS.cmd'
 
 $FilePath = "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Startup\IAS.cmd"
 
 try {
-    # Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
     $webClient = New-Object System.Net.WebClient
     $webClient.DownloadFile($DownloadURL, $FilePath)
 } catch {
